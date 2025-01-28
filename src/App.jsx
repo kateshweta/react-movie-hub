dd267b4596d7c0aa33a5107849df26badaec5186
shweta-kate <kateshweta17@gmail.com>
2025-01-28 17:57:35 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+       <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </main>
+  )
+}
+
+export default App
